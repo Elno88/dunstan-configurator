98c393bb28e2a6b6969ae5e5f5d09be2a12a8b0c
David Villa <david@kfoobar.se>
2023-03-02 10:30:41 +0100

    Add more recipients to farm insurance send_contact

--- app/Steps/Farminsurance/Kontakt.php
+++ app/Steps/Farminsurance/Kontakt.php
@@ -1,21 +1,18 @@
-<?php namespace App\Steps\Farminsurance;
+<?php
 
-use App\Http\Controllers\Controller;
+namespace App\Steps\Farminsurance;
+
 use App\Libraries\Focus\FocusApi;
 use App\Libraries\Focus\FocusApiException;
 use App\Libraries\Mailchimp\MailchimpApi;
 use App\Libraries\Mailchimp\MailchimpApiException;
-use App\Steps\StepInterface;
+use App\Mail\GardsforsakingContact;
+use App\Mail\GardsforsakingOffert;
 use App\Steps\StepAbstract;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 use Propaganistas\LaravelPhone\PhoneNumber;
-use App\Mail\GardsforsakingOffert;
-use App\Mail\GardsforsakingContact;
-
-use PDF;
-use Validator;
-use Mail;
 
 class Kontakt extends StepAbstract
 {
@@ -25,12 +22,11 @@ class Kontakt extends StepAbstract
 
     public function view(Request $request)
     {
-
         // Get data
         $farminsurance_step = $this->get_data('gardsforsakring.gardsforsakring') ?? 'gardsforsakring-a-1';
         $pdf = $this->get_data('gardsforsakring-b-2.pdf_data') ?? null;
 
-        if($farminsurance_step == 'gardsforsakring-a-1'){
+        if ($farminsurance_step == 'gardsforsakring-a-1') {
             $address_street = $this->get_data('gardsforsakring-a-1.street') ?? '';
             $address_zip = $this->get_data('gardsforsakring-a-1.zip') ?? '';
             $address_city = $this->get_data('gardsforsakring-a-1.city') ?? '';
@@ -42,44 +38,42 @@ class Kontakt extends StepAbstract
 
         return view('steps.farminsurance.kontakt', [
             'farminsurance_step' => $farminsurance_step,
-            'address_street' => $address_street,
-            'address_zip' => $address_zip,
-            'address_city' => $address_city,
-            'pdf' => $pdf
+            'address_street'     => $address_street,
+            'address_zip'        => $address_zip,
+            'address_city'       => $address_city,
+            'pdf'                => $pdf
         ]);
     }
 
     public function validateStep(Request $request)
     {
-        // Get data
         $farminsurance_step = $this->get_data('gardsforsakring.gardsforsakring') ?? 'gardsforsakring-a-1';
 
-        // input
         $input = [
-            'email'         => $request->get('email'),
-            'firstname'     => $request->get('firstname'),
-            'lastname'      => $request->get('lastname'),
-            'zip'           => $request->get('zip'),
-            'street'        => $request->get('street'),
-            'city'          => $request->get('city'),
-            'phone'         => $request->get('phone'),
-            'phone_time'    => $request->get('phone_time'),
-            'term'          => $request->get('term'),
+            'email'      => $request->get('email'),
+            'firstname'  => $request->get('firstname'),
+            'lastname'   => $request->get('lastname'),
+            'zip'        => $request->get('zip'),
+            'street'     => $request->get('street'),
+            'city'       => $request->get('city'),
+            'phone'      => $request->get('phone'),
+            'phone_time' => $request->get('phone_time'),
+            'term'       => $request->get('term'),
         ];
 
         $rules = [
-            'email'     => 'required|email',
-            'phone'     => 'nullable',
+            'email'      => 'required|email',
+            'phone'      => 'nullable',
             'phone_time' => 'nullable',
-            'firstname' => 'required',
-            'lastname'  => 'required',
-            'street'    => 'required',
-            'zip'       => 'required',
-            'city'      => 'required',
-            'term'      => 'required',
+            'firstname'  => 'required',
+            'lastname'   => 'required',
+            'street'     => 'required',
+            'zip'        => 'required',
+            'city'       => 'required',
+            'term'       => 'required',
         ];
 
-        if(isset($input['phone']) && !empty($input['phone'])){
+        if (isset($input['phone']) && !empty($input['phone'])) {
             $rules['phone'] = 'required';
             $rules['phone_time'] = 'required';
         }
@@ -89,16 +83,16 @@ class Kontakt extends StepAbstract
         // Validate bankaccount against focus api
         $validator->after(function ($validator) use (&$input) {
             // Validera telefonnummer
-            if(isset($input['phone']) && !empty($input['phone'])){
+            if (isset($input['phone']) && !empty($input['phone'])) {
                 try {
                     $input['phone'] = PhoneNumber::make($input['phone'], 'SE')->formatE164();
-                } catch(\Exception $e){
+                } catch (\Exception $e) {
                     $validator->errors()->add('phone', 'Du måste ange korrekt format på telefonnumret');
                 }
             }
         });
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             $response = [
                 'status' => 0,
                 'errors' => $validator->errors()->toArray()
@@ -111,14 +105,13 @@ class Kontakt extends StepAbstract
         // Send to focus
         try {
 
-            if($farminsurance_step == 'gardsforsakring-a-1'){
+            if ($farminsurance_step == 'gardsforsakring-a-1') {
                 // a, new
                 $this->send_contact($input);
             } else {
                 // b, insurley
                 $this->send_focus_data();
             }
-
         } catch (FocusApiException $e) {
             report($e);
             throw $e;
@@ -126,22 +119,21 @@ class Kontakt extends StepAbstract
 
         // Add to mailchimp
         try {
-
             // meta first and lastname, address
             $meta = [
-                'FNAME' => $input['firstname'] ?? '',
-                'LNAME' => $input['lastname'] ?? '',
+                'FNAME'   => $input['firstname'] ?? '',
+                'LNAME'   => $input['lastname'] ?? '',
                 'ADDRESS' => [
                     'addr1' => $input['street'] ?? '',
                     'addr2' => '',
-                    'zip' => $input['zip'] ?? '',
-                    'city' => $input['city'] ?? '',
+                    'zip'   => $input['zip'] ?? '',
+                    'city'  => $input['city'] ?? '',
                     'state' => '',
                 ]
             ];
 
             // Phone
-            if(isset($input['phone']) && !empty($input['phone'])){
+            if (isset($input['phone']) && !empty($input['phone'])) {
                 $meta['PHONE'] = $input['phone'];
                 $meta['TELEFONTID'] = $input['phone_time'] ?? '';
             }
@@ -149,66 +141,61 @@ class Kontakt extends StepAbstract
             $mailchimpapi = new MailchimpApi;
             $mailchimpapi->subscribe_member($input['email'], $meta);
 
-            if($farminsurance_step == 'gardsforsakring-a-1'){
+            if ($farminsurance_step == 'gardsforsakring-a-1') {
                 // manual
                 $mailchimpapi->member_assign_tags($input['email'], ['Offertförfrågan WEB', 'LeadGardKONTAKTA']);
             } else {
                 // jämför
                 $mailchimpapi->member_assign_tags($input['email'], ['Offertförfrågan WEB', 'LeadGardJAMFOR']);
             }
-
         } catch (MailchimpApiException $e) {
             report($e);
         }
 
         $response = [
-            'status'=> 1,
+            'status'    => 1,
             'next_step' => 'gardsforsakring-tack'
         ];
 
         return response()->json($response);
-
     }
 
-    // a
-    public function send_contact($data) {
-
+    public function send_contact($data)
+    {
         try {
-
             $send_emails_to = [];
 
-            if(config('services.focus.live')){
+            if (config('services.focus.live')) {
                 $send_email_to[] = config('services.dunstan.email_test');
                 $send_email_to[] = 'daniel.lazic@convertor.se';
+                $send_email_to[] = 'info@dunstan.se';
+                $send_email_to[] = 'petru.gilezan@dunstan.se';
+                $send_email_to[] = 'klas.redemo@dunstan.se';
+                $send_email_to[] = 'pernilla.ivarsson@dunstan.se';
+                $send_email_to[] = 'christine.wikstrand@dunstan.se';
             } else {
                 $send_email_to[] = config('services.dunstan.email_test');
             }
 
-            foreach($send_email_to as $email_to){
-                // Dunstan
+            foreach ($send_email_to as $email_to) {
                 Mail::to($email_to)->send(new GardsforsakingContact(
                     'Intresseanmälan Gårdsförsäkring',
                     $data
                 ));
             }
-
         } catch (\Exception $e) {
-            // Silent error if mail fails
             report($e);
         }
-
     }
 
-    // b
     public function send_focus_data()
     {
-
         $data = [
-            'insurances' => $this->get_data('gardsforsakring-b-2.insurances') ?? [],
+            'insurances'   => $this->get_data('gardsforsakring-b-2.insurances') ?? [],
             'civic_number' => $this->get_data('gardsforsakring-b-2.civic_number') ?? null,
-            'email' => $this->get_data('gardsforsakring-kontakt.email') ?? null,
-            'phone' => $this->get_data('gardsforsakring-kontakt.phone') ?? null,
-            'phone_time' => $this->get_data('gardsforsakring-kontakt.phone_time') ?? null,
+            'email'        => $this->get_data('gardsforsakring-kontakt.email') ?? null,
+            'phone'        => $this->get_data('gardsforsakring-kontakt.phone') ?? null,
+            'phone_time'   => $this->get_data('gardsforsakring-kontakt.phone_time') ?? null,
         ];
 
         // Generate pdf file from insurley insurance data
@@ -218,10 +205,11 @@ class Kontakt extends StepAbstract
 
         // Update or create customer with details
         $customer_data = [];
-        if(isset($data['email']) && !empty($data['email'])){
+
+        if (isset($data['email']) && !empty($data['email'])) {
             $customer_data['email'] = $data['email'];
         }
-        if(isset($data['phone']) && !empty($data['phone'])){
+        if (isset($data['phone']) && !empty($data['phone'])) {
             $customer_data['mobil'] = $data['phone'];
             $customer_data['telefon'] = $data['phone'];
         }
@@ -229,70 +217,57 @@ class Kontakt extends StepAbstract
         // First get customer
         $create_customer = false;
         try {
-            // get customer
             $focus_customer_response = $focusapi->get_customer($data['civic_number']);
         } catch (FocusApiException $e) {
             // probably meants customer doesnt exist, create it?
             $create_customer = true;
         }
 
-        if($create_customer){
-            // create customer
+        if ($create_customer) {
             $focus_customer = $focusapi->create_customer($data['civic_number'], $customer_data);
         } else {
-            // update customer
             $focus_customer = $focusapi->update_customer($data['civic_number'], $customer_data);
         }
 
-        // Get customer id
         $customer_id = $focus_customer['id'];
 
         $comment = 'Web';
 
-        if(isset($data['phone']) && !empty($data['phone'])){
-            $comment = 'Kund önskar att bli kontaktad mellan kl: '.($data['phone_time'] ?? '');
+        if (isset($data['phone']) && !empty($data['phone'])) {
+            $comment = 'Kund önskar att bli kontaktad mellan kl: ' . ($data['phone_time'] ?? '');
         }
 
-        // Create ticket
         $focus_ticket = $focusapi->create_ticket($customer_id, 'Offertunderlag gård', $comment);
         $focus_ticket_id = $focus_ticket['id'] ?? null;
 
-        // Upload pdf
-        if(!empty($focus_ticket_id)){
+        if (!empty($focus_ticket_id)) {
             try {
-
-                $encoded_file = 'data:application/pdf;base64,'.base64_encode($pdf_file);
+                $encoded_file = 'data:application/pdf;base64,' . base64_encode($pdf_file);
                 $focus_ticket_file = $focusapi->add_ticket_file($focus_ticket_id, 'Insurely.pdf', $encoded_file);
             } catch (FocusApiException $e) {
                 report($e);
             }
         }
 
-        // Send mail
         try {
-
             $send_emails_to = [];
-            if(config('services.focus.live')){
+
+            if (config('services.focus.live')) {
                 $send_email_to[] = config('services.dunstan.email_test');
                 $send_email_to[] = 'daniel.lazic@convertor.se';
             } else {
                 $send_email_to[] = config('services.dunstan.email_test');
             }
 
-            foreach($send_email_to as $email_to){
-                // Dunstan
+            foreach ($send_email_to as $email_to) {
                 Mail::to($email_to)->send(new GardsforsakingOffert(
                     'Offertunderlag gård',
                     [],
                     $pdf_file
                 ));
             }
-
         } catch (\Exception $e) {
-            // Silent error if mail fails
             report($e);
         }
-
     }
-
 }
