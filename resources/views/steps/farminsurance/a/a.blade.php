1dde86dc66ed6c21ffc9f23438f5701e8716a663
Mikael NIlsson <mikael.nilsson@convertor.se>
2023-04-20 11:37:09 +0200

    Update a.blade.php

--- resources/views/steps/farminsurance/a/a.blade.php
+++ resources/views/steps/farminsurance/a/a.blade.php
@@ -1,19 +1,30 @@
 <div class="frame">
     <div class="frame-contents">
 
+    	<div class="frame-caption">
+            <h1>Försäkra din hästgård</h1>
+            <ul class="frame-caption-usps">
+            	<li>Heltäckande skydd</li>
+            	<li>Skräddarsydd för din verksamhet</li>
+            	<li>Kunskap som ger poäng</li>
+            </ul>
+        </div>
+
+	<!--
         <div class="bubble bubble-type-a left">
             <div class="bubble-contents">
             	<i class="bubble-help btn-sidebar" data-content="gardsforsakring">?</i>
                 <p><span style="font-weight:600;">Nu börjar vi!</span> Vi kan antingen hämta dina nuvarande försäkringsuppgifter och återkomma med ett prisförslag, eller så kontaktar vi dig och tar det därifrån. Valet är ditt!</p>
             </div>
         </div>
+    -->
 
         <div class="bubble bubble-type-b input-radio left">
             <input id="gard-b" type="radio" class="btn-select" name="gardsforsakring" value="gardsforsakring-b-1">
             <label class="bubble-contents" for="gard-b">
                 <div class="stroke-left">
-                    <h3>Jämför din gårdsförsäkring</h3>
-                    <p>Vi hämtar dina nuvarande försäkringsuppgifter digitalt och återkommer med ett detaljerat prisförslag. Snabbt och enkelt!</p>
+                    <h3>Jämför din hästgårdsförsäkring</h3>
+                    <p>Se prisjämförelse och få en anpassad offert.</p>
                 </div>
             </label>
         </div>
@@ -22,8 +33,8 @@
             <input id="gard-a" type="radio" class="btn-select" name="gardsforsakring" value="gardsforsakring-a-1">
             <label class="bubble-contents" for="gard-a">
                 <div class="stroke-left">
-                    <h3>Ny gårdsförsäkring</h3>
-                    <p>För dig som inte har någon gårdsförsäkring idag och vill bli kontaktad av Dunstans försäkringsrådgivare.</p>
+                    <h3>Teckna ny hästgårdsförsäkring</h3>
+                    <p>Prata med våra rådgivare och få en detaljerad offert.</p>
                 </div>
             </label>
         </div>
