59177137496a7872a85c33c00c457108bf2a5fe3
David Villa <david@kfoobar.se>
2023-07-07 12:38:06 +0200

    PSR-2/12 formatting

--- app/Steps/Trailerinsurance/Tack.php
+++ app/Steps/Trailerinsurance/Tack.php
@@ -11,6 +11,13 @@ class Tack extends StepAbstract
     public $progressbar = 100;
     public $skipable = false;
 
+    /**
+     * Shows the step/page.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return Illuminate\View\View
+     */
     public function view(Request $request)
     {
         return view('steps.trailerinsurance.tack', [
@@ -18,6 +25,13 @@ class Tack extends StepAbstract
         ]);
     }
 
+    /**
+     * Validates the step.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function validateStep(Request $request)
     {
         return response()->json([
