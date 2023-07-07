8b5e5f017b01903a5b74b7c9cd691d1f16703de1
David Villa <david@kfoobar.se>
2023-07-07 12:36:04 +0200

    Add business logic for step 1-3

--- app/Steps/Trailerinsurance/A/A2.php
+++ app/Steps/Trailerinsurance/A/A2.php
@@ -2,15 +2,25 @@
 
 namespace App\Steps\Trailerinsurance\A;
 
+use App\Libraries\Focus\FocusApi;
+use App\Libraries\Focus\FocusApiException;
 use App\Steps\StepAbstract;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class A2 extends StepAbstract
 {
     public $name = 'trailerforsakring-a2';
-    public $progressbar = 50;
+    public $progressbar = 48;
     public $skipable = false;
 
+    /**
+     * Shows the step/page.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return Illuminate\View\View
+     */
     public function view(Request $request)
     {
         return view('steps.trailerinsurance.a.a2', [
@@ -20,9 +30,41 @@ class A2 extends StepAbstract
 
     public function validateStep(Request $request)
     {
+        $ssn = preg_replace('~\D~', '', $request->get('ssn'));
+
+        $validator = Validator::make(
+            [
+                'ssn' => $ssn,
+            ],
+            [
+                'ssn' => [
+                    'required',
+                    'regex:/(^[\d]{12}$)/u',
+                    'bail'
+                ]
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'  => 0,
+                'display' => 1,
+                'errors'  => $validator->errors()->toArray()
+            ]);
+        }
+
+        try {
+            $customer = (new FocusApi)->get_customer($ssn);
+        } catch (FocusApiException $e) {
+            $customer = null;
+        }
+
+        $this->store_data($ssn, 'ssn');
+        $this->store_data($customer, 'customer');
+
         return response()->json([
             'status'    => 1,
-            'next_step' => 'trailerforsakring-a3',
+            'next_step' => 'trailerforsakring-resultat',
         ]);
     }
 }
