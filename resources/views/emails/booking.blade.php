604cf5d659f91954af43431ec3323d1b1c296f0b
Alp Altunel <alpaltunel@hotmail.com>
2023-07-20 10:26:38 +0300

    restored correct version of new insurance

--- resources/views/emails/booking.blade.php
+++ resources/views/emails/booking.blade.php
@@ -1,6 +1,15 @@
-Nytecknad trailerförsäkring via web.<br/>
+Nytecknad försäkring via web.<br/>
 <br/>
 Kund #{{ $details['kundnr'] ?? '' }}<br/>
 {{ $details['efternamn'] ?? '' }}, {{ $details['fornamn'] ?? '' }}<br/>
 <br/>
 Försäkring som tecknats över webben måste kontrolleras innan den aktiveras och faktura skickas.<br/>
+<ul>
+    <li>Gå igenom hästens hälsoformulär.</li>
+    <li>Sök upp hästens ID-nummer i Blå basen, SWB, Jordbruksverket, och/eller Travsport.</li>
+    <li>Ange färg på hästen.</li>
+    <li>Ange korsning om det är fallet.</li>
+    <li>Kontrollera om hästen ska omfattas av Trygghetgarantin.</li>
+    <li>Kontakta kunden om uppgifter saknas eller inte är sökbara.</li>
+    <li>Är försäkringen korrekt ifylld av kund fyller du i en notering i Focus att allt är OK.</li>
+</ul>
