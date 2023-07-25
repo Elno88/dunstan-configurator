ef1e941dfde1042e7bb654801152393422192439
Alp Altunel <alpaltunel@hotmail.com>
2023-07-25 11:21:36 +0300

    fixed a problem with stefan's ssn

--- app/Steps/Trailerinsurance/A/A2.php
+++ app/Steps/Trailerinsurance/A/A2.php
@@ -54,33 +54,19 @@ class A2 extends StepAbstract
         }
 
         // First get customer
-        $create_customer = false;
+        $customer = null;
         $focusApi = new FocusApi();
         try {
             $customer = $focusApi->get_customer($ssn);
         } catch (FocusApiException $e) {
-            // probably means customer doesnt exist, create it?
-            $create_customer = true;
-        }
+            try {
+                $new_customer = $focusApi->get_address($ssn);
+                $customer['kund'] = $new_customer;
+                $customer['kund']['typ'] = 'person';
+                $customer['kund']['namn'] = $new_customer['fornamn'] . ' ' . $new_customer['efternamn'];
+            } catch (FocusApiException $e) {
 
-        try {
-            $new_customer = $focusApi->get_address($ssn);
-            $customer['kund'] = $new_customer;
-            $customer['kund']['typ'] = 'person';
-            $customer['kund']['namn'] = $new_customer['fornamn'] . ' ' . $new_customer['efternamn'];
-        } catch (FocusApiException $e) {
-            $customer = null;
-        }
-
-        if ($create_customer)
-        {
-            // create customer
-            $focus_customer = $focusApi->create_customer($ssn, $customer);
-        }
-        else
-        {
-            // update customer
-            $focus_customer = $focusApi->update_customer($ssn, $customer);
+            }
         }
 
         $this->store_data($ssn, 'ssn');
