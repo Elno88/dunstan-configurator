b7ef5d3bffbe2bf9d76f504c3b6a4c9169f6713c
Mikael NIlsson <mikael.nilsson@convertor.se>
2023-10-27 06:41:06 +0200

    frondend juderingar
    - Cookies link mobil.
    - "Ingen fast självrisk" bort.
    - Uppdaterad tabell resultat för trav mobil.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,5 +2,6 @@
 	<div class="footer-contents">
 		<button type="button" class="btn-back" title="Tillbaka"><i class="fa fa-chevron-left" aria-hidden="true"></i> Tillbaka</button>
 		<!--<button type="button" class="btn-reset" title="Börja om"><span class="ri ri-redo"></span> Börja om</button>-->
+    <span class="cookies-link" onclick="javascript:CookieConsent.renew();" onkeypress="javascript:CookieConsent.renew();">Cookieinställningar</span>
 	</div>
 </footer>
