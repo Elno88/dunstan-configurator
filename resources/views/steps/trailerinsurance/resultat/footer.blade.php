b11f1c4de61e129a16c0d8452c0266de31a9a80e
David Villa <david@kfoobar.se>
2023-11-24 09:35:06 +0100

    Add links to footer (#62)
    * Add links to footer
    
    * Fix faulty links

--- resources/views/steps/trailerinsurance/resultat/footer.blade.php
+++ resources/views/steps/trailerinsurance/resultat/footer.blade.php
@@ -1,1 +1,19 @@
-<div id="resultat-footer" style="display: block;"></div>
+<div id="resultat-footer" style="display: block;">
+    <div class="row row-3">
+        <h2>Våra villkor</h2>
+        <div class="info-columns">
+            <h3 class="col col-heading col-vet col-liv include">
+                Dunstan Hästtrailerförsäkring
+            </h3>
+            <a class="col col-vet col-liv include" href="https://dunstan.se/wp-content/uploads/villkor/website/dunstan_villkor-hasttrailer_2023-07-01.pdf" target="_blank">
+                <p>Försäkringsvillkor</p>
+            </a>
+            <a class="col col-vet col-liv include" href="https://dunstan.se/wp-content/uploads/villkor/website/dunstan_forefterkop-hasttrailer_2023-07-01.pdf" target="_blank">
+                <p>För- & efterköpsinformation</p>
+            </a>
+            <a class="col col-vet col-liv include" href="https://dunstan.se/wp-content/uploads/villkor/website/dunstan_produktblad-hasttrailer_2023-07-01.pdf" target="_blank">
+                <p>Produktblad</p>
+            </a>
+        </div>
+    </div>
+</div>
